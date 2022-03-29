246a301375c1aa5da58b2c591c61fef6adb95d10
praveen <rompicharlapraveen29@gmail.com>
2022-03-29 19:15:56 +0530

    Hello

--- DataModel/Controllers/EmployeeController.cs
+++ DataModel/Controllers/EmployeeController.cs
@@ -1,6 +1,5 @@
 ﻿using DataModel.Models;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 using static DataModel.Models.Employee;
@@ -10,16 +9,40 @@ namespace DataModel.Controllers
     public class EmployeeController : Controller
     {
         private EmpDBContext db = new EmpDBContext();
+
         // GET: Employee
-
-        public ActionResult Index()
+        public ActionResult Index(string sortOrder)
         {
-            var employees = from e in db.Employees
-                            orderby e.ID
-                            select e;
-            return View(employees);
-        }
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "Name_desc" : "";
+            ViewBag.JoiningDateSortParm = sortOrder == "JoiningDate" ? "JoiningDate_desc" : "JoiningDate";
+            ViewBag.AgeSortParm = sortOrder == "Age" ? "Age_desc" : "Age";
 
+            var employees = from employee in db.Employees
+                           select employee;
+            switch (sortOrder)
+            {
+                case "Name":
+                    employees = employees.OrderByDescending(employee => employee.Name);
+                    break;
+                case "JoiningDate":
+                    employees = employees.OrderBy(employee => employee.JoiningDate);
+                    break;
+                case "JoiningDate_desc":
+                    employees = employees.OrderByDescending(employee => employee.JoiningDate);
+                    break;
+                case "Age":
+                    employees = employees.OrderBy(employee => employee.Age);
+                    break;
+                case "Age_desc":
+                    employees = employees.OrderByDescending(employee => employee.Age);
+                    break;
+                default:
+                    employees = employees.OrderBy(employee => employee.Name);
+                    break;
+            }
+            return View(employees.ToList());
+        }
+ 
         // GET: Employee/Create
         public ActionResult Create()
         {
@@ -69,161 +92,40 @@ namespace DataModel.Controllers
                 return View();
             }
         }
+        // GET: Employee/Details/5
+        public ActionResult Details(int ? id)
+        {
+            var employee = db.Employees.Single(m => m.ID == id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(employee);
+            }
+        }
 
-        //        public static List<Employee> empList = new List<Employee>
-        //        {
-        //   new Employee{
-        //      ID = 1,
-        //      Name = "Praveen",
-        //      JoiningDate = DateTime.Parse(DateTime.Today.ToString()),
-        //      Age = 23
-        //   },
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
 
-        //   new Employee{
-        //      ID = 2,
-        //      Name = "Sasi",
-        //      JoiningDate = DateTime.Parse(DateTime.Today.ToString()),
-        //      Age = 24
-        //   },
-
-        //   new Employee{
-        //      ID = 3,
-        //      Name = "Sai",
-        //      JoiningDate = DateTime.Parse(DateTime.Today.ToString()),
-        //      Age = 25
-        //   },
-
-        //   new Employee{
-        //      ID = 4,
-        //      Name = "Madhu",
-        //      JoiningDate = DateTime.Parse(DateTime.Today.ToString()),
-        //      Age = 22
-        //   },
-
-        //};
-
-        //        public ActionResult Index()
-        //        {
-        //            var employees = from e in empList
-        //                            orderby e.ID
-        //                            select e;
-        //            return View(employees);
-        //        }
-        //        [NonAction]
-        //        public List<Employee> GetEmployeeList()
-        //        {
-        //            return new List<Employee>{
-        //      new Employee{
-        //         ID = 1,
-        //         Name = "Praveen",
-        //         JoiningDate = DateTime.Parse(DateTime.Today.ToString()),
-        //         Age = 23
-        //      },
-
-        //       new Employee{
-        //      ID = 2,
-        //      Name = "Sasi",
-        //      JoiningDate = DateTime.Parse(DateTime.Today.ToString()),
-        //      Age = 24
-        //   },
-
-        //   new Employee{
-        //      ID = 3,
-        //      Name = "Sai",
-        //      JoiningDate = DateTime.Parse(DateTime.Today.ToString()),
-        //      Age = 25
-        //   },
-
-        //   new Employee{
-        //      ID = 4,
-        //      Name = "Madhu",
-        //      JoiningDate = DateTime.Parse(DateTime.Today.ToString()),
-        //      Age = 22
-        //   },
-
-        //   };
-        //        }
-
-        //        // GET: Employee/Details/5
-        //        public ActionResult Details(int id)
-        //        {
-        //            return View();
-        //        }
-
-        //        // GET: Employee/Create
-        //        public ActionResult Create()
-        //        {
-        //            return View();
-        //        }
-
-        //        // POST: Employee/Create
-        //        [HttpPost]
-        //        public ActionResult Create(FormCollection collection)
-        //        {
-        //            try
-        //            {
-        //                Employee emp = new Employee();
-        //                emp.Name = collection["Name"];
-        //                DateTime jDate;
-        //                DateTime.TryParse(collection["DOB"], out jDate);
-        //                emp.JoiningDate = jDate;
-        //                string age = collection["Age"];
-        //                emp.Age = Int32.Parse(age);
-        //                empList.Add(emp);
-        //                return RedirectToAction("Index");
-        //            }
-        //            catch
-        //            {
-        //                return View();
-        //            }
-        //        }
-        //        // GET: Employee/Edit/5
-        //        public ActionResult Edit(int id)
-        //        {
-        //            List<Employee> empList = GetEmployeeList();
-        //            var employee = empList.Single(m => m.ID == id);
-        //            return View(employee);
-        //        }
-
-        //        // POST: Employee/Edit/5
-        //        [HttpPost]
-        //        public ActionResult Edit(int id, FormCollection collection)
-        //        {
-        //            try
-        //            {
-        //                var employee = empList.Single(m => m.ID == id);
-        //                if (TryUpdateModel(employee))
-        //                {
-        //                    //To Do:- database code
-        //                    return RedirectToAction("Index");
-        //                }
-        //                return View(employee);
-        //            }
-        //            catch
-        //            {
-        //                return View();
-        //            }
-        //        }
-
-        //        // GET: Employee/Delete/5
-        //        public ActionResult Delete(int id)
-        //        {
-        //            return View();
-        //        }
-
-        //        // POST: Employee/Delete/5
-        //        [HttpPost]
-        //        public ActionResult Delete(int id, FormCollection collection)
-        //        {
-        //            try
-        //            {
-        //                // TODO: Add delete logic here
-        //                return RedirectToAction("Index");
-        //            }
-        //            catch
-        //            {
-        //                return View();
-        //            }
-        //        }
+        // POST: Employee/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                var employee = db.Employees.Single(m => m.ID == id);
+                db.Employees.Remove(employee);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
     }
 }
